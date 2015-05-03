34f66ca1fe0d09e11af4ac882c18e8c71dc7d044
Zeno Rocha <hi@zenorocha.com>
2015-05-03 14:25:05 -0700

    Separates concerns in two different promise-based functions

--- controllers/find-id.js
+++ controllers/find-id.js
@@ -1,35 +1,54 @@
-var es = require('../configs/es');
+var boom = require('boom');
 var joi = require('joi');
+var es = require('../configs/es');
 
 function controller(request, reply) {
-    var id = request.params.id,
-        result = {};
+    controller.validate(request)
+        .then(function(result) {
+            console.log('[#validate] Done with promise');
+            return controller.findByID(result);
+        })
+        .then(function(result) {
+            console.log('[#findByID] Done with promise');
+            return reply(result);
+        })
+        .catch(reply);
+}
 
-    joi.validate(
-        { id: id },
-        { id: joi.number() },
-    function (err, value) {
-        if (err) {
-            reply(err);
-            return;
-        }
-
-        var esObject = {
-            index: 'customelements',
-            type: 'repo',
-            id: id
+controller.validate = function(request) {
+    return new Promise(function(resolve, reject) {
+        var params = {
+            id: request.params.id
         };
 
-        es.get(esObject).then(function (body) {
-            result.type = 'success';
-            result.result = body._source;
-            reply( result );
-        }, function (error) {
-            result.type = 'error';
-            result.message = error.message;
-            reply(result);
+        var schema = {
+            id: joi.number()
+        };
+
+        joi.validate(params, schema, function (err, result) {
+            if (err) {
+                reject(boom.badRequest(err));
+            }
+
+            resolve(result);
         });
     });
-}
+};
+
+controller.findByID = function(params) {
+    return new Promise(function(resolve, reject) {
+        var options = {
+            index: 'customelements',
+            type: 'repo',
+            id: params.id
+        };
+
+        es.get(options).then(function(body) {
+            resolve(body._source);
+        }, function (error) {
+            reject(boom.create(error.status, error.message));
+        });
+    });
+};
 
 module.exports = controller;
