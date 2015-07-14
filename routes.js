f402235dba31b88047f426da2401f23a45dcb4ae
Zeno Rocha <hi@zenorocha.com>
2015-07-14 10:22:51 -0700

    Removes unused routes

--- routes.js
+++ routes.js
@@ -13,35 +13,5 @@ module.exports = [
         method: 'GET',
         path: '/owners/{ownerID}',
         handler: require('./controllers/owners/owner/get.js')
-    },
-    {
-        method: 'GET',
-        path: '/id/{id}',
-        handler: require('./controllers/id/get.js')
-    },
-    {
-        method: 'POST',
-        path: '/id/{id}',
-        handler: require('./controllers/id/post.js')
-    },
-    {
-        method: 'PUT',
-        path: '/id/{id}',
-        handler: require('./controllers/id/put.js')
-    },
-    {
-        method: 'DELETE',
-        path: '/id/{id}',
-        handler: require('./controllers/id/delete.js')
-    },
-    {
-        method: 'GET',
-        path: '/name/{owner}/{repo}',
-        handler: require('./controllers/name/get.js')
-    },
-    {
-        method: 'GET',
-        path: '/owner/{owner}',
-        handler: require('./controllers/owner/get.js')
     }
 ];
