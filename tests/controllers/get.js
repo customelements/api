36066203e44b3924e129760162bc4d3c79d55669
Giovanni keppelen <keppelen.as@gmail.com>
2015-07-18 14:36:46 -0300

    more tests

--- tests/controllers/get.js
+++ tests/controllers/get.js
@@ -2,13 +2,40 @@ var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 
 var assert = require('chai').assert;
+var expect = require('chai').expect;
 var server = require('../../server');
 
-lab.experiment('GET /', function() {
+lab.experiment('Get a single owner', function() {
     lab.test('should return HTTP 200 status code', function(done) {
         var options = {
             method: 'GET',
-            url: '/'
+            url: '/owners/robdodson'
+        };
+
+        server.inject(options, function(response) {
+            assert.equal(response.statusCode, 200);
+            done();
+        });
+    });
+
+    lab.test('have property "name" in response', function(done) {
+        var options = {
+            method: 'GET',
+            url: '/owners/robdodson'
+        };
+
+        server.inject(options, function(response) {
+            expect(response.result).to.have.property('name');
+            done();
+        });
+    });
+});
+
+lab.experiment('List owner repositories', function() {
+    lab.test('should return HTTP 200 status code', function(done) {
+        var options = {
+            method: 'GET',
+            url: '/repos/robdodson'
         };
 
         server.inject(options, function(response) {
