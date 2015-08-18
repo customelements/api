595b174147c59f829edfb1a372cb5e35f8b2eed4
Zeno Rocha <hi@zenorocha.com>
2015-08-18 12:34:53 -0700

    Moving from Found.no to Digital Ocean lol

--- configs/env.js
+++ configs/env.js
@@ -1,6 +1,6 @@
 module.exports = function() {
     if (!process.env.SECRET_TOKEN ||
-        !process.env.FOUNDELASTICSEARCH_URL) {
+        !process.env.ELASTIC_URL) {
         throw new Error('Please setup environment vars');
     }
 }();
